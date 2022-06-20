c6157242df7398ae9edecd5f581d579957c8e657
FlavioVillar <flavio_villar@hotmail.com>
2022-06-20 20:01:03 -0300

    feat: add router get talker by id - Requisito 2 - Crie o endpoint GET /talker/:id

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -11,5 +11,18 @@ router.get('/', async (_req, res) => {
 }
 );
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await readFile();
+    const talker = data.find(talker => talker.id === Number(id));
+
+    if (!talker) {
+        res.status(404).json({ message: "Pessoa palestrante não encontrada" });
+    }
+
+    return res.status(200).json(talker);
+}
+);
+
 
 module.exports = router;
